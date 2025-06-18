1b1fa010b1537897f963599e0461d2fd4a006c89
Jamkris <dltmdgus1412@gmail.com>
2025-06-18 17:11:14 +0900

    완성

--- routes/Auth/KakaoAuth/auth.js
+++ routes/Auth/KakaoAuth/auth.js
@@ -8,7 +8,7 @@ passport.use(
 		{
 			clientID: process.env.KAKAO_CLIENT_ID,
 			clientSecret: process.env.KAKAO_CLIENT_SECRET,
-			callbackURL: `${process.env.CLIENT_ORIGIN}/auth/kakao/callback`,
+			callbackURL: `${process.env.SERVER_ORIGIN}/auth/kakao/callback`,
 			passReqToCallback: true,
 		},
 		async (request, accessToken, refreshToken, profile, done) => {
