6b6d4092c450842a58b1a88b3f29b03688df36b6
Jamkris <dltmdgus1412@gmail.com>
2025-07-02 11:18:55 +0900

    CORS 에러 수정

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 
 app.use(express.json());
+app.use(cors());
 
 //Port Setting
 const PORT = process.env.PORT;
